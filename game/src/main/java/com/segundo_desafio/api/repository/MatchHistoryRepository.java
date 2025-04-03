0b93ef0e49c4ffb35131ef043cae9a20aa2d880e
lucca.valadares <lucca_laxusbr15@hotmail.com>
2025-04-03 15:35:17 -0300

    feat(repository): criar repositório para persistência de partidas

--- game/src/main/java/com/segundo_desafio/api/repository/MatchHistoryRepository.java
+++ game/src/main/java/com/segundo_desafio/api/repository/MatchHistoryRepository.java
@@ -1,6 +1,6 @@
 package com.segundo_desafio.api.repository;
 
-import com.segundo_desafio.api.domain.matchHistory.MatchHistory;
+import com.segundo_desafio.api.domain.matchhistory.MatchHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
