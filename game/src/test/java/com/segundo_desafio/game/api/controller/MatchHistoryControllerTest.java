54517423843ebaf2af304c34c6efd86467fad93e
lucca.valadares <lucca_laxusbr15@hotmail.com>
2025-04-08 12:07:11 -0300

    test:  implementação do teste do MatchHistoryController

--- game/src/test/java/com/segundo_desafio/game/api/controller/MatchHistoryControllerTest.java
+++ game/src/test/java/com/segundo_desafio/game/api/controller/MatchHistoryControllerTest.java
@@ -1,34 +1,25 @@
 package com.segundo_desafio.game.api.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.segundo_desafio.game.domain.dto.MatchHistoryResponseDTO;
 import com.segundo_desafio.game.domain.service.MatchHistoryService;
-import com.segundo_desafio.game.persistence.model.MatchHistory;
-import com.segundo_desafio.game.persistence.repository.MatchHistoryRepository;
-import net.bytebuddy.asm.Advice;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-
-
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
+
 
 
 @ExtendWith(SpringExtension.class)
@@ -39,33 +30,40 @@ class MatchHistoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @InjectMocks
+    @MockBean
     MatchHistoryService matchHistoryService;
 
-    @Mock
-    private MatchHistoryRepository matchHistoryRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
 
     @Test
     void shouldReturnHistoryWhenMatchHistoryExists() throws Exception {
-
-        MatchHistory matchHistory = new MatchHistory();
-        matchHistory.setPlayerOneMove("ROCK");
-        matchHistory.setPlayerTwoMove("PAPER");
-        matchHistory.setPlayerOneName("player1");
-        matchHistory.setPlayerTwoName("player2");
-
         MatchHistoryResponseDTO match1 = new MatchHistoryResponseDTO(
-                "ROCK", "player1", "PAPER", "player2", LocalDateTime.now(), "PLAYER_TWO_WINS");
+            "ROCK", "player1", "PAPER", "player2", LocalDateTime.now(), "PLAYER_TWO_WINS");
 
-        List<MatchHistoryResponseDTO> matchHistoryList = List.of(match1);
+        List<MatchHistoryResponseDTO> expectedList = List.of(match1);
 
-        when(matchHistoryRepository.findAll()).thenReturn(List.of(new MatchHistory()));
-        when(matchHistoryService.getAllRounds()).thenReturn(matchHistoryList);
+        given(matchHistoryService.getAllRounds()).willReturn(expectedList);
 
-        mockMvc.perform(get("/api/game/history"))
+        // Realizar a requisição e validar a resposta
+        mockMvc.perform(get("/api/game/history")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].playerOneMove").value("ROCK"))
                 .andExpect(jsonPath("$[0].playerTwoMove").value("PAPER"))
-                .andExpect(jsonPath("$[0].result").value("DRAW"));
+                .andExpect(jsonPath("$[0].result").value("PLAYER_TWO_WINS"));
+    }
+
+    @Test
+    void shouldReturnEmptyHistoryWhenMatchHistoryDoesNotExist() throws Exception{
+
+        given(matchHistoryService.getAllRounds()).willReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/game/history")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isEmpty());
     }
 }
