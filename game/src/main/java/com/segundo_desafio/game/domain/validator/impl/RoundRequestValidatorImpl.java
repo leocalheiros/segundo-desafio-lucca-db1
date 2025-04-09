2fea4c2a5e9dc57d14009bb26e69080fb6bd36c3
lucca.valadares <lucca_laxusbr15@hotmail.com>
2025-04-09 10:18:45 -0300

    refactor: utiliza o isNotBlank do StringUtils para fazer a validação

--- game/src/main/java/com/segundo_desafio/game/domain/validator/impl/RoundRequestValidatorImpl.java
+++ game/src/main/java/com/segundo_desafio/game/domain/validator/impl/RoundRequestValidatorImpl.java
@@ -3,6 +3,7 @@ package com.segundo_desafio.game.domain.validator.impl;
 import com.segundo_desafio.game.api.model.exception.InvalidGameRequestException;
 import com.segundo_desafio.game.domain.dto.RoundRequestDTO;
 import com.segundo_desafio.game.domain.validator.RoundRequestValidator;
+import io.micrometer.common.util.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,10 +11,10 @@ public class RoundRequestValidatorImpl implements RoundRequestValidator {
 
     @Override
     public void validate(RoundRequestDTO dto){
-        if(dto.playerOneName() == null || dto.playerOneName().isEmpty()){
+        if(!StringUtils.isNotBlank(dto.playerOneName())){
             throw new InvalidGameRequestException("Player one name cannot be null or empty");
         }
-        if(dto.playerTwoName() == null || dto.playerTwoName().isEmpty()){
+        if(!StringUtils.isNotBlank(dto.playerTwoName())){
             throw new InvalidGameRequestException("Player two name cannot be null or empty");
         }
     }
