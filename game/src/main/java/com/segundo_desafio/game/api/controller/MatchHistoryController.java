9e01ea1a788c43de6b7bedabebef1487b2896ace
lucca.valadares <lucca_laxusbr15@hotmail.com>
2025-04-11 15:03:15 -0300

    refactor: adicona a rota "padrão" nos properties e logs nos controllers

--- game/src/main/java/com/segundo_desafio/game/api/controller/MatchHistoryController.java
+++ game/src/main/java/com/segundo_desafio/game/api/controller/MatchHistoryController.java
@@ -2,6 +2,10 @@ package com.segundo_desafio.game.api.controller;
 
 import com.segundo_desafio.game.domain.dto.MatchHistoryResponseDTO;
 import com.segundo_desafio.game.domain.service.MatchHistoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
-@RequestMapping("/api/game")
+@RequestMapping("/history")
+@RequiredArgsConstructor
 public class MatchHistoryController {
 
 
     private final MatchHistoryService matchHistoryService;
+    private final Logger logger = LogManager.getLogger(MatchHistoryService.class.getName());
 
-    public MatchHistoryController(MatchHistoryService service){
-        this.matchHistoryService = service;
-    }
 
-    @GetMapping("/history")
+
+    @GetMapping
     public ResponseEntity<List<MatchHistoryResponseDTO>> getAll(){
-        return ResponseEntity.ok(matchHistoryService.getAllRounds());
+
+        List<MatchHistoryResponseDTO> res = matchHistoryService.getAllRounds();
+        logger.info(res);
+
+        return ResponseEntity.ok(res);
     }
 }
--- game/src/main/java/com/segundo_desafio/game/api/controller/RoundController.java
+++ game/src/main/java/com/segundo_desafio/game/api/controller/RoundController.java
@@ -3,7 +3,11 @@ package com.segundo_desafio.game.api.controller;
 
 import com.segundo_desafio.game.domain.dto.RoundRequestDTO;
 import com.segundo_desafio.game.domain.dto.RoundResponseDTO;
+import com.segundo_desafio.game.domain.service.MatchHistoryService;
 import com.segundo_desafio.game.domain.service.RoundService;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,20 +15,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/game")
+@RequestMapping("/play")
+@RequiredArgsConstructor
 public class RoundController {
 
-
     private final RoundService roundService;
+    private final Logger logger = LogManager.getLogger(MatchHistoryService.class.getName());
 
-    public RoundController (RoundService roundService){
-        this.roundService = roundService;
-    }
-
-    @PostMapping("/play")
+    @PostMapping
     public ResponseEntity<RoundResponseDTO> startGame(@RequestBody RoundRequestDTO body){
 
-        return ResponseEntity.ok(roundService.saveRound(body));
+        RoundResponseDTO res = roundService.saveRound(body);
+        logger.info(res);
+
+        return ResponseEntity.ok(res);
     }
 
 }
